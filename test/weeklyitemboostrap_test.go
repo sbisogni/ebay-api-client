e2608a778598f417c2917c693fadbd48f8a0da41
Simone Bisogni <simone_bisogni@yahoo.it>
2020-05-15 17:44:39 +0200

    integration test WeekItemBoostrapt

--- test/weeklyitemboostrap_test.go
+++ test/weeklyitemboostrap_test.go
@@ -1,18 +1,50 @@
 package test
 
 import (
+	"compress/gzip"
+	"context"
+	"ebay-api-client/ebay"
+	"ebay-api-client/oauth2"
+	"io/ioutil"
+	"os"
 	"testing"
+
+	"gotest.tools/v3/assert"
 )
 
 func Test_WeekItemBoostrap(t *testing.T) {
 
-	// clientID := os.Getenv("EBAY_CLIENTID")
-	// clientSecret := os.Getenv("EBAY_CLIENTSECRET")
-	// scope := []string{"https://api.ebay.com/oauth/api_scope",
-	// "https://api.ebay.com/oauth/api_scope/buy.item.feed"}
+	filename := "feed.tzv.gz"
 
-	// client := oauth2.NewClient(ebay.NewSandboxTokenSource(clientID, clientSecret, scope))
-	// eBayClient := ebay.NewSandboxClient(client)
-	// ebayClient.WeeklyItemBoostrap()
+	ctx := context.Background()
+	httpClient, err := oauth2.NewSandboxClientCredentialsClient(ctx, []string{oauth2.ScopeBuyFeedAPI})
+	assert.NilError(t, err)
 
+	feedClient := ebay.NewSandboxFeedService(httpClient)
+	//feedClient.ChunkSize = 1816
+
+	feed, err := os.Create(filename)
+	assert.NilError(t, err)
+
+	defer feed.Close()
+	defer os.Remove(filename)
+
+	info, err := feedClient.WeeklyItemBoostrap(ctx, "EBAY_US", "1", feed)
+
+	assert.NilError(t, err)
+	assert.Assert(t, info.Size != 0)
+
+	// Preparing for reading
+	feed.Seek(0, 0)
+
+	gunzip, err := gzip.NewReader(feed)
+	assert.NilError(t, err)
+
+	gunzip.Multistream(false)
+
+	b, err := ioutil.ReadAll(gunzip)
+	assert.NilError(t, err)
+
+	assert.Assert(t, len(b) != 0)
+	t.Logf(string(b))
 }
